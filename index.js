9c756f6df13230320d77d071a038585b4df3e483
anna3bars <154560345+anna3bars@users.noreply.github.com>
2025-03-16 18:02:06 +0400

    changed the url of qr

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ let lastValidToken = null;
 // Генерация QR-кода в памяти// Երբ QR-ը թարմացվում է, պահում ենք նոր token-ը
 async function generateQRCode() {
     lastValidToken = Date.now().toString(); // Նոր վավեր token
-    const qrURL = `http://127.0.0.1:5500/validate?token=${lastValidToken}`;
+    const qrURL = `http://127.0.0.1:5500/qr-video/index.html?token=${lastValidToken}`;
 
     const qrBuffer = await QRCode.toBuffer(qrURL);
     try {
